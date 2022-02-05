0874cf1e656b78650161fa716148aab2150fd369
Jonathan Gamble <jig1314@gmail.com>
2022-02-05 12:00:36 -0500

    Reverting SQLite code

--- TraCR/Server/Program.cs
+++ TraCR/Server/Program.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.EntityFrameworkCore;
-using TraCR.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
 var app = builder.Build();
 
--- TraCR/Shared/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TraCR.Shared.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-    }
-}
