80e152950b5a122d1e850f6b34316c36ada8f984
Jonathan Gamble <jig1314@gmail.com>
2022-02-22 20:13:29 -0500

    Added TabBlazor Template

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tracr.Client;
+using Tracr.Client.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,7 +13,6 @@ builder.Services.AddHttpClient("Tracr.ServerAPI", client => client.BaseAddress =
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Tracr.ServerAPI"));
-
 builder.Services.AddApiAuthorization();
 
 await builder.Build().RunAsync();
