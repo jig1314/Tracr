8b722de3cc681925f142ce4ffe1af9c12e5dc496
Jonathan Gamble <jig1314@gmail.com>
2022-03-02 00:42:03 -0500

    Added Register Page and Blazorstrap references

---
+++ src/Client/Pages/Register.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Tracr.Client.ViewModels;
+
+namespace Tracr.Client.Pages
+{
+    public partial class Register : ComponentBase
+    {
+        [Parameter]
+        public string ReturnUrl { get; set; } = "";
+
+        [Inject]
+        public NavigationManager? NavigationManager { get; set; }
+
+        public RegisterViewModel RegisterViewModel { get; set; } = new RegisterViewModel();
+
+        public string ErrorMessage { get; set; } = "";
+
+        public bool SubmittingData { get; set; } = false;
+
+        protected override void OnInitialized()
+        {
+            
+        }
+
+        protected async Task RegisterUser()
+        {
+            
+        }
+    }
+}
