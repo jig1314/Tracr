2a201894121cbf158eef30b6184854a7657762bd
Jonathan Gamble <jig1314@gmail.com>
2022-03-02 14:16:16 -0500

    Added Login and Reset Password pages

---
+++ src/Client/Pages/ResetPassword.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Tracr.Client.ViewModels;
+
+namespace Tracr.Client.Pages
+{
+    public partial class ResetPassword : ComponentBase
+    {
+        public ResetPasswordViewModel ResetPasswordViewModel { get; set; } = new ResetPasswordViewModel();
+
+        public string ErrorMessage { get; set; } = "";
+
+        public bool SubmittingData { get; set; } = false;
+
+        protected async Task ResetUserPassword()
+        {
+            
+        }
+    }
+}
