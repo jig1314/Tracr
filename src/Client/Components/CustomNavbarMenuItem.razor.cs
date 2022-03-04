50de7b4a5854e516628d3f8d511737583ab6ece8
Jonathan Gamble <jig1314@gmail.com>
2022-03-04 14:35:48 -0500

    Adding mockup for user profile page

---
+++ src/Client/Components/CustomNavbarMenuItem.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using TabBlazor;
+
+namespace Tracr.Client.Components
+{
+    public partial class CustomNavbarMenuItem : NavbarMenuItem
+    {
+        [Parameter] public bool IsActive { get; set; } = false;
+
+        protected string CustomClassNames => base.ClassBuilder
+            .Add(base.ClassNames)
+            .AddIf("active", !IsDropdown && IsActive)
+            .ToString();
+    }
+}
+
---
+++ src/Client/Pages/PersonalInfo.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Tracr.Client.ViewModels;
+
+namespace Tracr.Client.Pages
+{
+    public partial class PersonalInfo : ComponentBase
+    {
+
+        public PersonalInfoViewModel PersonalInfoViewModel { get; set; } = new PersonalInfoViewModel();
+
+        public string ErrorMessage { get; set; } = "";
+
+        public bool SubmittingData { get; set; } = false;
+
+        protected async Task UpdateInformation()
+        {
+            
+        }
+    }
+}
