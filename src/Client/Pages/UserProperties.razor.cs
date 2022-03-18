4f54a7563366d02861589a6633f3e38ee1a1e2e3
Jonathan Gamble <jig1314@gmail.com>
2022-03-18 02:17:08 -0400

    Added property list page to user profile

---
+++ src/Client/Pages/UserProperties.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Tracr.Shared.DTOs;
+
+namespace Tracr.Client.Pages
+{
+    public  partial class UserProperties : ComponentBase
+    {
+        [CascadingParameter]
+        public Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+
+        public AuthenticationState? AuthenticationState { get; set; }
+
+        [Inject]
+        public NavigationManager? NavigationManager { get; set; }
+
+        protected List<PropertyDto> UserPropertyDtos { get; set; } = new List<PropertyDto>();
+
+        public string ErrorMessage { get; set; } = "";
+
+        public bool RetrievingData { get; set; } = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (AuthenticationStateTask == null || NavigationManager == null)
+                return;
+
+            AuthenticationState = await AuthenticationStateTask;
+
+            if (AuthenticationState.User.Identity == null || !AuthenticationState.User.Identity.IsAuthenticated)
+            {
+                NavigationManager.NavigateTo($"/login/{Uri.EscapeDataString(NavigationManager.Uri)}");
+            }
+            else
+            {
+                // TODO: Add Data Retrieval
+            }
+        }
+    }
+}
