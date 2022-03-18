a47e4df6f045a9622898847f2def32ad8d4b624d
Jonathan Gamble <jig1314@gmail.com>
2022-03-17 22:53:56 -0400

    Added Property Schema and Data Models

---
+++ src/Server/Models/Property.cs
@@ -0,0 +1,19 @@
+﻿namespace Tracr.Server.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int NumBedrooms { get; set; }
+
+        public int NumBathrooms { get; set; }
+
+        public Mortage Mortage { get; set; }
+
+        public Address Address { get; set; }
+
+        public List<Renter> Renters { get; set; }
+    }
+}
