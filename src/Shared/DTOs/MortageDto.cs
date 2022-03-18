eeb1f6815b7565b8187e89f7ce15b2c956fbf9eb
Jonathan Gamble <jig1314@gmail.com>
2022-03-17 23:27:11 -0400

    Add Property API Controller and a method to get a user's properties

---
+++ src/Shared/DTOs/MortageDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Tracr.Shared.DTOs
+{
+    public class MortageDto
+    {
+        public int PropertyId { get; set; }
+
+        public decimal Principal { get; set; }
+
+        public decimal MonthlyPayment { get; set; }
+
+        public decimal APR { get; set; }
+    }
+}
