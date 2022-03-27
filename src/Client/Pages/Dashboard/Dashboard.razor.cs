1ea682de0caad13dd89df4465f12899410e7dd01
Jonathan Gamble <jig1314@gmail.com>
2022-03-27 15:22:49 -0400

    Wired up ProjectedProfit Dashboard Tile

--- src/Client/Pages/Dashboard/Dashboard.razor.cs
+++ src/Client/Pages/Dashboard/Dashboard.razor.cs
@@ -1,11 +1,94 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Tracr.Client.Services;
+using Tracr.Shared.DTOs;
+using Tracr.Shared.Models;
 
 namespace Tracr.Client.Pages.Dashboard
 {
     public partial class Dashboard : ComponentBase
     {
+        [CascadingParameter]
+        public Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+
+        public AuthenticationState? AuthenticationState { get; set; }
+
         [Inject]
         public NavigationManager? NavigationManager { get; set; }
+
+        [Inject]
+        public IPropertyService? PropertyService { get; set; }
+
+        public string ErrorMessage { get; set; } = "";
+
+        public bool LoadingData { get; set; } = false;
+
+        protected List<PropertyDto> UserPropertyDtos { get; set; }
+
+        protected List<PropertyIncome> PropertyIncome { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (AuthenticationStateTask == null || NavigationManager == null || PropertyService == null)
+                return;
+
+            AuthenticationState = await AuthenticationStateTask;
+
+            if (AuthenticationState.User.Identity == null || !AuthenticationState.User.Identity.IsAuthenticated)
+            {
+                NavigationManager.NavigateTo($"/login/{Uri.EscapeDataString(NavigationManager.Uri)}");
+            }
+            else
+            {
+                await GetUserProperties();
+                await RefreshUserPropertyIncome();
+            }
+        }
+
+        private async Task GetUserProperties()
+        {
+            if (PropertyService == null)
+                return;
+
+            ErrorMessage = "";
+
+            try
+            {
+                LoadingData = true;
+                UserPropertyDtos = await PropertyService.GetUserProperties();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = $"{ex.Message}";
+            }
+            finally
+            {
+                LoadingData = false;
+            }
+        }
+
+        private async Task RefreshUserPropertyIncome()
+        {
+            if (PropertyService == null)
+                return;
+
+            ErrorMessage = "";
+
+            try
+            {
+                LoadingData = true;
+
+                PropertyIncome = await PropertyService.GetUserPropertyIncome();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = $"{ex.Message}";
+            }
+            finally
+            {
+                LoadingData = false;
+            }
+        }
 
         protected void GoToManageProperties()
         {
--- src/Client/Pages/Dashboard/UserPropertyDashboardTile.razor.cs
+++ src/Client/Pages/Dashboard/UserPropertyDashboardTile.razor.cs
@@ -12,37 +12,7 @@ namespace Tracr.Client.Pages.Dashboard
         [Inject]
         public IPropertyService? PropertyService { get; set; }
 
-        protected List<PropertyDto> UserPropertyDtos { get; set; } = new List<PropertyDto>();
-
-        public string ErrorMessage { get; set; } = "";
-
-        public bool LoadingUserPropertyData { get; set; } = false;
-
-        protected override async Task OnInitializedAsync()
-        {
-            await GetUserProperties();
-        }
-
-        private async Task GetUserProperties()
-        {
-            if (PropertyService == null)
-                return;
-
-            ErrorMessage = "";
-
-            try
-            {
-                LoadingUserPropertyData = true;
-                UserPropertyDtos = await PropertyService.GetUserProperties();
-            }
-            catch (Exception ex)
-            {
-                ErrorMessage = $"{ex.Message}";
-            }
-            finally
-            {
-                LoadingUserPropertyData = false;
-            }
-        }
+        [Parameter]
+        public List<PropertyDto> UserProperties { get; set; }
     }
 }
