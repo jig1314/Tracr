7e520874927385c9567ae62cb3f2e975718c6e5d
Alexandria Fuller <alexfuller123@yahoo.com>
2022-04-10 03:11:00 -0400

    Added Cancel button to Property.razor and Property.razor.cs

--- src/Client/Pages/Property.razor.cs
+++ src/Client/Pages/Property.razor.cs
@@ -231,6 +231,32 @@ namespace Tracr.Client.Pages
             }
         }
 
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (unsavedChanges)
+            {
+                var result = MessageBox.Show("Save changes?", "unsaved changes", MessageBoxButtons.YesNoCancel);
+
+                if (result == DialogResult.Yes)
+                {
+                    SaveChanges();
+                }
+
+                if (result == DialogResult.Cancel)
+                {
+                    result = DialogResult.None;
+                }
+
+                this.DialogResult = result;
+            }
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = cancelClose;
+            cancelClose = false;
+        }
+
         protected async Task DeleteRenter(int renterId)
         {
             if (PropertyService == null)
